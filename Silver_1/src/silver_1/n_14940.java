c03c320b28398064a1b7fdacdad6b39fd7d65c08
SeoulMephist <mephistowaltz22@gmail.com>
2024-08-08 14:49:37 +0900

    10810

---
+++ Silver_1/src/silver_1/n_14940.java
@@ -0,0 +1,11 @@
+package silver_1;
+import java.io.*;
+
+public class n_14940 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+    }
+}
